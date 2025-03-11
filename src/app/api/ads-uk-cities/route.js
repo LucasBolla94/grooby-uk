125be1c0a5ffe5f501ca12325c2a29980ba2a96b
Lucas <lucasbolla@icloud.com>
2025-03-11 14:47:21 +0000

    V 0.0.01.60

--- src/app/api/ads-uk-cities/route.js
+++ src/app/api/ads-uk-cities/route.js
@@ -2,14 +2,14 @@ import { NextResponse } from 'next/server';
 
 // Simulação de um banco de dados ou Firestore (substitua conforme necessário)
 const mockCities = [
-  { id: 1, name: 'London' }
+  { id: 1, name: 'Glasgow' }
 ];
 
 export async function GET() {
   try {
     return NextResponse.json({ cities: mockCities });
   } catch (error) {
-    console.error('Erro ao buscar cidades:', error);
+    console.error('Error to find the cities:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
